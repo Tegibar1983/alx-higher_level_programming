ce9cb182ee05d99798a7593b3784a6f2173d717e
Tegibar1983 <tbtf1991@gmail.com>
2023-10-07 02:47:02 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,5 +4,5 @@ def multiple_returns(sentence):
     if sentence != '':
         first_char = sentence[0]
     else:
-        first_char = none
+        first_char = None
     return (len(sentence), first_char)
