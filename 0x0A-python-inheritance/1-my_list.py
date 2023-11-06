fbd185c3924ca4571d2402c4cbc47b2c9506102d
Tegibar1983 <tbtf1991@gmail.com>
2023-11-06 11:52:43 -0800

    2-is_same_class.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module inherits from the list class"""
+"""Checks if object is an instance of a class"""
 
 
-class MyList(list):
-    """A class that inherits from list"""
-    def print_sorted(self):
-        """prints a sorted list"""
-        print(sorted(self))
+def is_same_class(obj, a_class):
+    """Return true if object is an instance of the
+    class, otherwise return false
+    """
+    return (type(obj) == a_class)
