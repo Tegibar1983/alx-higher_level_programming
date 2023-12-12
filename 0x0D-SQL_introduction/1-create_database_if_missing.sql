ee4c76149ed6ebafb79e0d773f919673e984fcfd
Tegibar1983 <tbtf1991@gmail.com>
2023-12-12 04:38:21 -0800

    1-create_database_if_missing.sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 -- create the database hbtn_0c_0 in your mySQL server.
 -- if the database hbtn_0c_0 exists, your script should not fail.
 -- you are not allowed to use SELECT or SHOW statements.
-CREATE DATABASE IF NOT EXITS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
