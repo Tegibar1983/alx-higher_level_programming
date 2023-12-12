7aff931f03b129f345e5799389c3ab367cac27af
Tegibar1983 <tbtf1991@gmail.com>
2023-12-12 04:57:43 -0800

    2-remove_database.sql

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- deletes the database hbtn_0c_0 in your mySQL server.
+-- if the database hbtn_0c_0 dase't exist, your script will fail.
+-- you are not allowed to use SELECT and SHOW statement.
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
