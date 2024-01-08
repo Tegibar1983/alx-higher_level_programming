2c5b2582c75200b2f75ccfa2b440d6ae8ff7755d
Tegibar1983 <tbtf1991@gmail.com>
2024-01-08 07:38:12 -0800

    js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const x = process.argv[2];
+
+if (!parseInt(x)){
+	console.log('Missing size');
+} else {
+for(i = 0; i < x; i++)
+let y = 0;
+let myVar = '';
+
+while (y < x){
+	myVar = myVarr + 'X';
+	y++;
+}
+console.log(myVar);
+}
+
+}
