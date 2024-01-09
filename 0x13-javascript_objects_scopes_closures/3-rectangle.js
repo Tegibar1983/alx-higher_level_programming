e273994f7c3f514f29f6ef7e1ff4cc86dd54f376
Tegibar1983 <tbtf1991@gmail.com>
2024-01-09 04:03:52 -0800

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,27 +1,26 @@
 #!/usr/bin/node
-
 /**
- * A rectangle class with parameteres provided
+ * Check the parameters provided
  */
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-class Rectangle(){
-	constructor(w, h){
-		if(typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0){
-			this.width = w;
-			this.height = h;
-		}
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
 
-	print(){
-		for(i = 0; i < this.height; i++){
-			let myVar = '';
-			let y = 0;
-			while(y < this.width){
-				myVar += 'x';
-				y++;
-			}
-			console.log(myVar);
-		}
-	}
+      console.log(myVar);
+    }
+  }
 }
 module.exports = Rectangle;
