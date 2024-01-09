1174549dc95c8a6afc7de50c971dcb1afbaea41f
Tegibar1983 <tbtf1991@gmail.com>
2024-01-09 01:25:37 -0800

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
-
 /**
- * Represents a rectangle class with parametres defind.
+ * Check the parameters provided
  */
-
 class Rectangle {
-	constructor (w, h) {
-		if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-
-	}
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 }
 module.exports = Rectangle;
