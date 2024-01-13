19e807f8a2f355df70ff74ca65261b9adfd02d7e
Tegibar1983 <tbtf1991@gmail.com>
2024-01-12 19:34:03 -0800

    JavaScript - Objects, Scopes and Closures

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+This scrips will list all state
+found in 'hbtn_0e_0_usa'
 """
-
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
+    """ Acess the database and get all the states
+    inside it;
     """
     db_connect = MySQLdb.connect(
-        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-
+            host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
-
-    db_cursor.execute("SELECT * FROM states")
-
+    db_cursor.execute('SELECT * FROM states')
     rows_selected = db_cursor.fetchall()
-
     for row in rows_selected:
         print(row)
+
