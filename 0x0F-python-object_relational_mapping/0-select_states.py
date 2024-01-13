dc30ef6ea035f17607d95583364b3ea9f726b29a
Tegibar1983 <tbtf1991@gmail.com>
2024-01-12 19:51:22 -0800

    JavaScript - Objects, Scopes and Closures

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,9 +11,9 @@ if __name__ == '__main__':
     inside it
     """
     db_connect = MySQLdb.connect(
-            host="localhost", usr=argv[1], port=3306, passwd=argv[2], db=argv[3])
+            host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
     db_cursor = db_connect.cursor()
-    db_cursor.execute('SELECT * FROM states')
+    db_cursor.execute("SELECT * FROM states")
     rows_selected = db_cursor.fetchall()
     for row in rows_selected:
         print(row)
