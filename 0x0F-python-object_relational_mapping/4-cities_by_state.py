cb91526ffc0eec9a9800402321730bc8d14c24de
Tegibar1983 <tbtf1991@gmail.com>
2024-01-13 21:15:39 -0800

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 """
-This a script lists all cities from
-the database `hbtn_0e_0_usa`
+This script lists all cities from
+the database `hbtn_0e_4_usa`.
 """
 
 import MySQLdb as db
@@ -10,11 +9,13 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Acess the database and get all
-    cities from the database.
+    Access the database and get the cities
+    from the database.
     """
-    db_connect = db.connect(
-            host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
     with db_connect.cursor() as db_cursor:
         db_cursor.execute("SELECT cities.id, cities.name, states.name \
                                 FROM cities JOIN states ON cities.state_id \
@@ -24,4 +25,3 @@ if __name__ == '__main__':
     if rows_selected is not None:
         for row in rows_selected:
             print(row)
-
