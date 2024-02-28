260ce7942b96cac817f019a73e5c70043c7d4c0f
Tegibar1983 <tbtf1991@gmail.com>
2024-02-28 04:59:29 -0800

    jquery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,9 @@
+// 100-script.js
+
+// Wait for the HTML content to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the <header> element and update its text color to red (#FF0000)
+  var headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
+
